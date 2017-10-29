440525ff09ec4dbd942f817a9aec203e7fd56906
Aris Aslani <arisaktien@gmail.com>
2017-10-29 15:10:36 +0100

    test: get stock price for apple

--- stock.py
+++ stock.py
@@ -1,1 +1,10 @@
 import pandas as pd
+import pandas_datareader.data as web
+import datetime
+
+start = datetime.datetime(2013, 1, 1)
+end = datetime.datetime(2015, 1, 1)
+
+f = web.DataReader("AAPL", 'google', start, end)
+
+print(f.ix['2013-01-02'])
